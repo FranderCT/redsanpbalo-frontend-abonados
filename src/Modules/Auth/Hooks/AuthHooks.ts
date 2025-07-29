e8b182a1360cd1e68e060702f961807236ffe92a
FranderCT <09.reelaborar-cerosa@icloud.com>
2025-07-29 10:07:09 -0600

    cambios

--- src/Modules/Auth/Hooks/AuthHooks.ts
+++ src/Modules/Auth/Hooks/AuthHooks.ts
@@ -1,1 +1,18 @@
- 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { registerUser } from '../Services/AuthServices';
+import type { userPruebaDTO, userPruebaForm } from '../Models/UserPrueba';
+
+export const useRegisterMutation = () => {
+  const queryClient = useQueryClient()
+    
+   const mutation = useMutation<userPruebaDTO, Error, userPruebaForm>({
+    mutationFn: registerUser,
+    onSuccess: (res) => {
+    console.log('Usuario registrado:', res);
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+    },
+  })
+
+  return mutation;
+};
