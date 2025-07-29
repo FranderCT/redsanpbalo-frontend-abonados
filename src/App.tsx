f951b4b2fb1cabba88c1dc55891db1ce013a940b
Brenda Obando <brenobandogo0805@gmail.com>
2025-07-28 22:24:16 -0600

    Muchos cambios-RegistroUsuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css'
+import RegisterUser from './components/RegisterUser';
 
-function App() {
+const queryClient = new QueryClient();
+
+export function App() {
 
   return (
-    <>
-      
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RegisterUser></RegisterUser>
+    </QueryClientProvider>
   )
 }
 
