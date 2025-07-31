e5dbc40069c7cc9c39b84eca510907ffc892410e
FranderCT <09.reelaborar-cerosa@icloud.com>
2025-07-31 14:54:40 -0600

    cambios

--- src/Modules/Lading/HeroPage.tsx
+++ src/Modules/Lading/HeroPage.tsx
@@ -1,9 +1,12 @@
+import Servicios from "./servicios"
+
 const HeroPage = () => {
 
   return (
     <div>
         <h1>hola mundo</h1>
-        <a href="/auth/login"></a>
+        <a href="/auth/login" > teoca para seguir </a>
+        <Servicios />
     </div>
   )
 }
