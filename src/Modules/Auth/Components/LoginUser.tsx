57e2ca993be4b698ff2e0e0658c6d34f5d041eec
FranderCT <09.reelaborar-cerosa@icloud.com>
2025-07-31 03:02:45 -0600

    cambios

--- src/Modules/Auth/Components/LoginUser.tsx
+++ src/Modules/Auth/Components/LoginUser.tsx
@@ -1,19 +1,29 @@
 import { useForm } from '@tanstack/react-form'
 import { AuthInitialState } from '../Models/Auth';
 import { useLogin } from '../Hooks/AuthHooks';
+import { AuthSchema } from '../schemas/AuthSchemas';
+import { toast } from 'react-toastify';
 
 
-const LoginPrueba = () => {
+const LoginUser = () => {
     const loginMutation = useLogin();
 
     const form = useForm({
        defaultValues : AuthInitialState,
+        validators: {
+            onChange: AuthSchema,
+        },
        onSubmit: async ({value}) => {
-        await loginMutation.mutateAsync(value);
-        console.log("Usuario logueado:", value);
+        try{
+            await loginMutation.mutateAsync(value);
+            console.log('Inicio de Sesión Exitoso');
+            toast.success('¡Acceso exitoso!', { position: "top-right", autoClose: 3000 });
+        }catch (error: any){
+            console.log('error')
+            toast.error('¡Acceso Denegado!', { position: "top-right", autoClose: 3000 });
+        }
        } 
-    });
-
+    }); 
 
   return (
     <div className="bg-white w-full max-w-4xl rounded-2xl shadow-lg p-6 flex flex-col md:flex-row gap-6 h-auto md:h-[600px] mx-auto">
@@ -54,26 +64,44 @@ const LoginPrueba = () => {
                 className="w-full max-w-md p-2 flex flex-col gap-4"
             >
                 <form.Field name="email">
-                {(field) => (
-                    <input
-                    className="w-full px-4 py-2 bg-gray-100 text-[#091540] rounded-md text-sm"
-                    value={field.state.value}
-                    placeholder="Escriba su Correo Electrónico"
-                    type="email"
-                    onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                )}
+                    {(field) => (
+                        <>
+                        <input
+                            type="email"
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            placeholder="Correo electrónico"
+                            className="w-full px-4 py-2 bg-gray-100 text-[#091540] rounded-md text-sm"
+                        />
+                        {field.state.meta.isTouched && field.state.meta.errors.length > 0 && (
+                            <p className="text-sm text-red-500 mt-1">
+                                {(field.state.meta.errors[0] as any)?.message ??
+                                String(field.state.meta.errors[0])}
+                            </p>
+                        )}
+                        </>
+                    )}
                 </form.Field>
+
 
                 <form.Field name="password">
                 {(field) => (
-                    <input
-                    className="w-full px-4 py-2 bg-gray-100 text-[#091540] rounded-md text-sm"
-                    placeholder="Escriba su Contraseña"
-                    value={field.state.value}
-                    type="password"
-                    onChange={(e) => field.handleChange(e.target.value)}
-                    />
+                    <>
+                        <input
+                            className="w-full px-4 py-2 bg-gray-100 text-[#091540] rounded-md text-sm"
+                            placeholder="Escriba su Contraseña"
+                            value={field.state.value}
+                            type="password"
+                            onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {field.state.meta.isTouched && field.state.meta.errors.length > 0 && (
+                            <p className="text-sm text-red-500 mt-1">
+                                {(field.state.meta.errors[0] as any)?.message ??
+                                String(field.state.meta.errors[0])}
+                            </p>
+                        )}
+                    </>
                 )}
                 </form.Field>
 
@@ -106,4 +134,4 @@ const LoginPrueba = () => {
   )
 }
 
-export default LoginPrueba
+export default LoginUser
--- src/Modules/Auth/Routes/authRoutes.ts
+++ src/Modules/Auth/Routes/authRoutes.ts
@@ -3,10 +3,8 @@ import { createRoute } from "@tanstack/react-router";
 
 import AuthLayout from "../Layouts/AuthLayout";
 import { rootRoute } from "../../../routes";
-
-import RegisterPrueba from "../Components/RegisterPrueba";
-import LoginPrueba from "../Components/Login";
 import RegisterAbonados from "../Components/RegisterAbonados";
+import LoginUser from "../Components/LoginUser";
 
 
 export const authRoute = createRoute({
@@ -25,7 +23,7 @@ export const registerAbonadoRoute = createRoute({
 export const loginRoute = createRoute({
    getParentRoute: () => authRoute,
    path: "login",
-   component: LoginPrueba,
+   component: LoginUser,
 })
 
 // /auth/register-user
