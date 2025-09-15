d3eccf8c40ce60737a20afd1b913dbb648cd2333
Brenda Obando <brenobandogo0805@gmail.com>
2025-09-14 23:01:08 -0600

    Carpetaprojects-model-projectroute

---
+++ src/Modules/Projects/Routes/ProjectsRoutes.tsx
@@ -0,0 +1,9 @@
+import { createRoute } from "@tanstack/react-router";
+import { dashboardRoute } from "../../Dashboard/Routes/DashboardRoutes";
+
+
+export const projectRoute = createRoute ({
+    getParentRoute : () => dashboardRoute,
+    path: 'projects',
+    // component: ListCategories
+})
