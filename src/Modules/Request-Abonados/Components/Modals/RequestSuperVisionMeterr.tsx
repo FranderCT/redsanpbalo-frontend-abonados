f8568b819f253f1285ef592ad735c9391cabe96b
saMujbo <samuelbarrantes1@gmail.com>
2025-10-31 13:06:34 -0600

    ww

--- src/Modules/Request-Abonados/Components/Modals/RequestSuperVisionMeterr.tsx
+++ src/Modules/Request-Abonados/Components/Modals/RequestSuperVisionMeterr.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ModalBase } from "../../../../Components/Modals/ModalBase";
 import { useTempSMLink } from "../../Hooks/Supervision-Meter/SupervisionMeterHookF";
 
