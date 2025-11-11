055312f8535c8465bada3617f35d4fe73f18627c
FranderCT <09.reelaborar-cerosa@icloud.com>
2025-11-11 12:13:35 -0600

    cambios

--- src/Modules/ProveedoresFisicos/Services/ProveedoresFisicosServices.ts
+++
@@ -1,7 +0,0 @@
-import apiAxios from "../../../api/apiConfig";
-import type { ProveedorFisico } from "../Models/ProveedorFisico";
-
-export async function createProveedorFisico(proveedor: ProveedorFisico): Promise<ProveedorFisico> {
-    const response = await apiAxios.post<ProveedorFisico>("/physical-supplier", proveedor);
-    return response.data;
-}
