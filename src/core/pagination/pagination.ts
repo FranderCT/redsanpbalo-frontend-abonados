b6c4192737875bb02de9c40fefa8be9e59924236
FranderCT <09.reelaborar-cerosa@icloud.com>
2026-03-01 19:21:01 -0600

    cambios

---
+++ src/core/pagination/pagination.ts
@@ -0,0 +1,14 @@
+export type PaginationMeta = {
+    totalItems: number;
+    itemCount: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+    hasNextPage: boolean;
+    hasPrevPage: boolean;
+};
+
+export type PaginatedResponse<T> = {
+    data: T[];
+    meta: PaginationMeta;
+};
