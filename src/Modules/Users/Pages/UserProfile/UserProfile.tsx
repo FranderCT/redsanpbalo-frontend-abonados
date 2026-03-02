9ca015a348e3fe51c4b4140744724576e9e30f97
FranderCT <09.reelaborar-cerosa@icloud.com>
2026-03-01 22:55:24 -0600

    cambios

--- src/Modules/Users/Pages/UserProfile/UserProfile.tsx
+++ src/Modules/Users/Pages/UserProfile/UserProfile.tsx
@@ -1,25 +1,26 @@
-import HeaderUserProfile from "../../Components/ProfileUser/HeaderUserProfile"
-import UserPhotoProfile from "../../Components/ProfileUser/UserPhotoProfile"
-import UserProfileDetails from "../../Components/ProfileUser/UserProfileDetails"
+import HeaderUserProfile from "../../Components/ProfileUser/HeaderUserProfile";
+import UserPhotoProfile from "../../Components/ProfileUser/UserPhotoProfile";
+import UserProfileDetails from "../../Components/ProfileUser/UserProfileDetails";
 import { useGetUserProfile } from "../../Hooks/UsersHooks";
-
+import { Separator } from "@/Components/ui/separator";
 
 const Profile = () => {
-  const {UserProfile} = useGetUserProfile();
-  return (
-    <main className="flex flex-col justify-center min-h-full w-full bg-[#F9F5FF]">
-      {/* Header */}
-      <HeaderUserProfile/>
-      <div className="border-b border-dashed border-gray-300 mb-8 mt-6"></div>
-      {/* Cards */}
-      <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* Foto de perfil */}
-        <UserPhotoProfile User={UserProfile}/>
-        {/* Información del usuario */}
-        <UserProfileDetails User={UserProfile}/> 
-      </section>
-    </main>
-  )
-}
+  const { UserProfile } = useGetUserProfile();
 
-export default Profile
+  return (
+    <main className="flex flex-col w-full min-h-full bg-background">
+      <div className="flex flex-col gap-6 p-4 sm:p-6 lg:p-8 max-w-5xl mx-auto w-full">
+        <HeaderUserProfile />
+
+        <Separator className="bg-border" />
+
+        <section className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <UserPhotoProfile User={UserProfile} />
+          <UserProfileDetails User={UserProfile} />
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
