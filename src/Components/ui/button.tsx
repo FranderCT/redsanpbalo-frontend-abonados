379e2eebe1912f2b24c181e06496cb30197bc5b2
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2026-03-02 15:18:20 -0600

    fix-outline button

--- src/Components/ui/button.tsx
+++ src/Components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input border-[#1789FC] bg-background text-[#1789FC] font-semibold shadow-sm hover:bg-[#1789FC] hover:text-accent-foreground hover:text-white",
+          "border border-input border-[#1789FC] bg-background text-[#1789FC] font-semibold shadow-sm hover:bg-[#1789FC] hover:text-white",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
